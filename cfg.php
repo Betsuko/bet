0dbb9540b8b15bfa40898588b21acf2f2249d1df
Betsuko <121996322+Betsuko@users.noreply.github.com>
2023-01-05 12:25:23 +0800

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,8 +1,8 @@
 <?php
-$proxy_ip = '45.94.47.66';
-$proxy_port = '8110';
-$Username = 'Cerina8099';
-$Password = 'Cerina8099';
-$cok = "PHPSESSID=hqnb2r1t5uoeg0dq0pmm27i5js; bitmedia_fid=eyJmaWQiOiIyZmM2ZDdlZjg2Y2EyNzg0MmVmOWMwYTE0MmMzZjMzMSIsImZpZG5vdWEiOiJiMDQ0NTgxOWNlOTBjYjEyYTg4ZjhkZTU4MTc4NTU5MiJ9; _gid=GA1.2.1245966529.1672678843; clever-last-tracker-53577=1; SesHashKey=0oeoeweqrkox8x6h; SesToken=ses_id=240169&ses_key=0oeoeweqrkox8x6h; AccExist=240169; uuid=Pcb35f84f-ecc8-409a-aa67-957f41e2c575; clever-counter-53577=0-1; _ga=GA1.2.1978538166.1672678842; _gat_gtag_UA_133726835_1=1; _ga_J2L7YD89W2=GS1.1.1672792363.3.1.1672792637.0.0.0";
-$us = "Mozilla/5.0(KHTML, like Gecko) Chrome/93.0.4577.ebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.107 Safari/537.36";
+$proxy_ip = '185.199.228.220';
+$proxy_port = '7300';
+$Username = 'Remunan88';
+$Password = 'Remunan88';
+$cok = "PHPSESSID=fie3qj5npa3ajucv5en3uuudpe; RefSource=https://r.search.yahoo.com/_ylt=AwrIQ2cTT7ZjTjMA3ztUACQ5;_ylu=Y29sbwMEcG9zAzEEdnRpZAMEc2VjA3Ny/RV=2/RE=1672920979/RO=10/RU=https%3a%2f%2fclaimbits.net%2f/RK=2/RS=80KBPZujun2CeKoqx9DT0tlb4SA-; _gid=GA1.2.721079494.1672892196; clever-last-tracker-53577=1; clever-counter-53577=0-1; bitmedia_fid=eyJmaWQiOiJiZmE3YjY2M2U2YzQzMThkMzViMTcyMjAyZTZiOWFkZCIsImZpZG5vdWEiOiJjODE1ZGNiMDhjZDBmYmViYmUwMDIxMTkxZTkyNGU4OCJ9; AccExist=242183; SesHashKey=zsusoymuwwxqfbu6; SesToken=ses_id=242183&ses_key=zsusoymuwwxqfbu6; cf_clearance=paiChdJhHKNbRoD3mtqJ81D52H0cNwVytVhn1ZIIcyw-1672892371-0-160; _ga_J2L7YD89W2=GS1.1.1672892191.1.1.1672892502.0.0.0; _ga=GA1.2.2063216898.1672892192";
+$us = "Mozilla/5.0 (Linux; Android 11; Pixel xiaomi) AppleWebKit/423.35 (KHTML, like Gecko) Chrome/101.0.348.131 Mobile Safari/421.76";
 
